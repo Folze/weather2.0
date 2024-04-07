b5b97e44f9d19f8e10a77bd2ef8c0f47eda93f96
Folze <thefolze@gmail.com>
2024-04-07 20:22:22 +0500

    change api to forecast

--- script.js
+++ script.js
@@ -1,47 +1,64 @@
 import { UI_ELEMENTS } from "./ui.js";
 
+const API_KEY = 'eed21df466d9691fdef055d585c190c7';
 
 async function showWeather(cityName) {
     try {
-
-        // Api
-        const serverUrl = 'http://api.openweathermap.org/data/2.5/weather';
-        const apiKey = 'eed21df466d9691fdef055d585c190c7';
-        const url = `${serverUrl}?q=${cityName}&appid=${apiKey}&units=metric`;
-
-        let response = await fetch(url);
-        let json = await response.json();
-
-        console.log(json)
-        const icon = json.weather[0].icon
+    const serverUrl = 'http://api.openweathermap.org/data/2.5/forecast';
+    const url = `${serverUrl}?q=${cityName}&appid=${API_KEY}&units=metric`; 
+    
+    const response = await fetch(url);
+    const jsonData = await response.json();
+    
+    console.log(jsonData) // check che prihodit
+    
+    // City Name
+    UI_ELEMENTS.currentCity.textContent = jsonData.city.name;
+    
+    // Закат,Восход
+    const sunset = new Date(jsonData.city.sunset * 1000)
+    const sunrise = new Date(jsonData.city.sunrise * 1000)
+    UI_ELEMENTS.sunset.textContent = `${sunset.getHours()}:${sunset.getMinutes() < 10 ? '0' + sunset.getMinutes() : sunset.getMinutes()}`;
+    UI_ELEMENTS.sunrise.textContent = `${sunrise.getHours()}:${sunrise.getMinutes() < 10 ? '0' + sunrise.getMinutes() : sunrise.getMinutes()}`;
+    
+    const forecastData = jsonData.list;
+    const currentTime = Math.floor(Date.now() / 1000);
+    let currentForecast = null;
+    
+    for (const forecast of forecastData) {
+        if (forecast.dt >= currentTime) {
+            currentForecast = forecast;
+            break;
+        }
+    }
+    
+        // Icon
+        const icon = currentForecast.weather[0].icon
         const iconUrl = `https://openweathermap.org/img/wn/${icon}@4x.png`;
-
-        // Восход,Заход
-        const sunset = new Date(json.sys.sunset * 1000)
-        const sunrise = new Date(json.sys.sunrise * 1000)
-
-        UI_ELEMENTS.sunset.textContent = `${sunset.getHours()}:${sunset.getMinutes() < 10 ? '0' + sunset.getMinutes() : sunset.getMinutes()}`;
-        UI_ELEMENTS.sunrise.textContent = `${sunrise.getHours()}:${sunrise.getMinutes() < 10 ? '0' + sunrise.getMinutes() : sunrise.getMinutes()}`;
-
+        UI_ELEMENTS.currentIcon.setAttribute('src', iconUrl);
 
         // Temp
-        const temperature = Math.round(json.main.temp);
-        const feelsLike = Math.round(json.main.feels_like);
+        const temperature = Math.round(currentForecast.main.temp);
+        const feelsLike = Math.round(currentForecast.main.feels_like);
         UI_ELEMENTS.temp.textContent = temperature + '°';
         UI_ELEMENTS.feelsLike.textContent = feelsLike + '°';
 
-        UI_ELEMENTS.currentCity.textContent = json.name;
-        UI_ELEMENTS.currentIcon.setAttribute('src', iconUrl);
-
-    } catch (err) {
-        throw new Error(alert(err));
+        
+        if (jsonData.cod === '404') {
+            alert('Город не найден');
+            return;
+        }
+    } catch (error) {
+      alert('Ошибка при получении погоды');
+     console.error(error);
+        }
     }
-}
 
 
 
 UI_ELEMENTS.searchForm.addEventListener('submit', (event) => {
     event.preventDefault();
+    
     const cityName = UI_ELEMENTS.inputCity.value;
     showWeather(cityName);
 });
@@ -75,7 +92,7 @@ UI_ELEMENTS.heart.addEventListener('click', (event) => {
 UI_ELEMENTS.favoriteCities.addEventListener('click', (event) => {
     event.preventDefault()
     renderCities()
-    showWeather(cityName.textContent)
+    // showWeather(cityName.textContent)
 })
 
 function renderCities() {
