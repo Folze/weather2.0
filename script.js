2a6b68c17e9c1eee8a2db3d7ec435b3afc335686
Folze <thefolze@gmail.com>
2024-04-17 22:16:47 +0500

    added Set()

--- script.js
+++ script.js
@@ -56,33 +56,30 @@ const year = currentDate.getFullYear()
 const month = `${currentDate.getMonth() + 1 < 10 ? '0' + (currentDate.getMonth() + 1) : currentDate.getMonth() + 1}`;
 const day = `${currentDate.getDate() < 10 ? '0' + currentDate.getDate() : currentDate.getDate()}`
 
-
+// Получение data на 12-15-18 часов
 const timeTwelve =`${year}-${month}-${day} 12:00:00`
 const timeFifteen =`${year}-${month}-${day} 15:00:00`
 const timeEighteen = `${year}-${month}-${day} 18:00:00`
 
-
-const dataTimeTwelve =forecastData.find(item=>item.dt_txt === timeTwelve)
-console.log(dataTimeTwelve)
-const tempTimeTwelve = Math.round(dataTimeTwelve.main.temp)
-TEMPERATURE.timeTwelve.textContent = tempTimeTwelve + '°';
-TEMPERATURE.feelsLikeTwelve.textContent = feelsLike + '°';
-
-
-const dataTimeFifteen =forecastData.find(item=>item.dt_txt === timeFifteen)
-const tempTimeFifteen = Math.round(dataTimeFifteen.main.temp)
-TEMPERATURE.timeFifteen.textContent = tempTimeFifteen + '°';
-TEMPERATURE.feelsLikeFifteen.textContent = feelsLike + '°';
-
-
-
-const dataTimeEighteen=forecastData.find(item=>item.dt_txt === timeEighteen)
-const tempTimeEighteen = Math.round(dataTimeEighteen.main.temp)
-TEMPERATURE.timeEighteen.textContent = tempTimeEighteen + '°';
-TEMPERATURE.feelsLikeEighteen.textContent = feelsLike + '°';
-
-
-
+// //12 часов
+// const dataTimeTwelve = forecastData.find(item=>item.dt_txt === timeTwelve)
+// console.log(dataTimeTwelve)
+// const tempTimeTwelve = Math.round(dataTimeTwelve.main.temp)
+// const feelsLikeTwelve = Math.round(dataTimeTwelve.main.feels_like)
+// TEMPERATURE.timeTwelve.textContent = tempTimeTwelve + '°';
+// TEMPERATURE.feelsLikeTwelve.textContent = feelsLikeTwelve + '°';
+// // 15 часов
+// const dataTimeFifteen =forecastData.find(item=>item.dt_txt === timeFifteen)
+// const tempTimeFifteen = Math.round(dataTimeFifteen.main.temp)
+// const feelsLikeFifteen= Math.round(dataTimeFifteen.main.feels_like)
+// TEMPERATURE.timeFifteen.textContent = tempTimeFifteen + '°';
+// TEMPERATURE.feelsLikeFifteen.textContent = feelsLikeFifteen + '°';
+// // 18 часов
+// const dataTimeEighteen=forecastData.find(item=>item.dt_txt === timeEighteen)
+// const tempTimeEighteen = Math.round(dataTimeEighteen.main.temp)
+// const feelsLikeEighteen = Math.round(dataTimeEighteen.main.feels_like)
+// TEMPERATURE.timeEighteen.textContent = tempTimeEighteen + '°';
+// TEMPERATURE.feelsLikeEighteen.textContent = feelsLikeEighteen + '°';
 
     } catch (error) {
       alert('Ошибка при получении погоды');
@@ -92,32 +89,25 @@ TEMPERATURE.feelsLikeEighteen.textContent = feelsLike + '°';
 
 UI_ELEMENTS.searchForm.addEventListener('submit', (event) => {
     event.preventDefault();
-    
     const cityName = UI_ELEMENTS.inputCity.value;
     showWeather(cityName);
 });
 
-
-// const city = UI_ELEMENTS.currentCity.textContent;
 let cities = JSON.parse(localStorage.getItem('cities')) || [];
-
-
 
 UI_ELEMENTS.heart.addEventListener('click', (event) => {
     event.preventDefault();
     const city = UI_ELEMENTS.currentCity.textContent;
-
-    if (!Array.isArray(cities)) {
-        cities = [];
-    }
-    if (!cities.includes(city)) {
-        cities.push(city);
-    }
-
-    if (cities.length >= 13) {
+    
+    
+    const testArr = new Set(cities);
+    testArr.add(city)
+    const filteredArr = Array.from(testArr)
+    if (filteredArr.length >= 13) {
         alert("Слишком много")
     } else {
-        localStorage.setItem('cities', JSON.stringify(cities));
+        localStorage.setItem('cities', JSON.stringify([...filteredArr]));
+        cities = [...filteredArr]
         renderCities()
     }
 })
@@ -126,7 +116,6 @@ UI_ELEMENTS.heart.addEventListener('click', (event) => {
 UI_ELEMENTS.favoriteCities.addEventListener('click', (event) => {
     event.preventDefault()
     renderCities()
-    // showWeather(cityName.textContent)
 })
 
 function renderCities() {
@@ -144,7 +133,6 @@ function renderCities() {
         cityName.classList.add("item-li");
         btnDeleteFavCity.classList.add("buttonDelete");
 
-        // favCityId.textContent = i
         cityName.textContent = city;
 
         UI_ELEMENTS.cityList.appendChild(newFavCity).appendChild(cityName).prepend(favCityId)
@@ -156,9 +144,6 @@ function renderCities() {
             event.preventDefault()
             showWeather(city)
         })
-
-
-
 
         // функция удаления из массива
         function deleteCity(city) {
